79b1ac222ce32ae829a65cc72871262fa4d177a4
Jacob Domagala <domagala.jacob@gmail.com>
2025-05-27 19:05:57 +0200

    [#14]: Lower learning rate

--- src/neuralnet.hpp
+++ src/neuralnet.hpp
@@ -35,10 +35,10 @@ struct Linear : Module {
 
   void gradient_update() const override {
     if (weights.grad().initialized())
-      launch_sgd_update(weights.data(), weights.grad().data(), 0.02f,
+      launch_sgd_update(weights.data(), weights.grad().data(), 0.001f,
                         weights.numel());
     if (bias.grad().initialized())
-      launch_sgd_update(bias.data(), bias.grad().data(), 0.02f, bias.numel());
+      launch_sgd_update(bias.data(), bias.grad().data(), 0.001f, bias.numel());
 
     weights.zero_grad();
     bias.zero_grad();
