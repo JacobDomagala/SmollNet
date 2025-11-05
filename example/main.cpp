34fe16b0033a55713f48155c561e634206d75ec0
JacobDomagala <domagala.jacob@gmail.com>
2025-11-05 09:32:48 +0100

    [#60]: Restore main.cpp and use manual_seed

--- example/main.cpp
+++ example/main.cpp
@@ -5,26 +5,25 @@
 using namespace smollnet;
 
 int main() {
-  constexpr int batch_size = 1024;
-  constexpr int num_features = 2048;
+  constexpr int input_size = 10;
 
   manual_seed(1234);
-  Tensor input = rand({batch_size, num_features}, DataType::f32, Device::CUDA);
- // input.print_elms();
-  
-  Tensor result = mse(input,input);
-  // auto targets_h = targets.cpu();
 
-  // auto net = Dense(Linear(num_features, 64), LayerNorm(), GeLU(), Linear(64, 1));
+  Tensor input = rand({input_size, 128}, DataType::f32, Device::CUDA);
+  Tensor targets = rand({input_size, 1}, DataType::f32, Device::CUDA);
+  auto targets_h = targets.cpu();
 
-  // for (int epoch = 0; epoch < 64; ++epoch) {
-  //   auto res = net.forward(input);
-  //   auto loss = mse(res, targets);
-  //   fmt::print("epoch[{}]: Loss={}\n", epoch, static_cast<float*>(loss.cpu().data())[0]);
-  //   loss.backward();
+  auto net = Dense(Linear(128, 64), LayerNorm(), GeLU(), Linear(64, 1));
 
-  //   auto optim = SGD(net.parameters(), 0.005f);
-  //   optim.step();
-  //   optim.zero_grad();
-  // }
+  for (int epoch = 0; epoch < 64; ++epoch) {
+    auto res = net.forward(input);
+    auto loss = mse(res, targets);
+    fmt::print("epoch[{}]: Loss={}\n", epoch, static_cast<float*>(loss.cpu().data())[0]);
+    loss.backward();
+
+    auto optim = SGD(net.parameters(), 0.005f);
+    optim.step();
+    optim.zero_grad();
+  }
 }
+
